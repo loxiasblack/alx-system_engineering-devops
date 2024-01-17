9d2c5043f4231d4f0709f37c319744a0070e6861
ilias <iliaschokairi@gmail.com>
2024-01-18 00:33:45 +0100

    try an other time

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
 # creat a new file
-file { '/tmp/school':
-    ensure  => 'file',
-    content => 'I love Puppet',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0774',
+node default {
+    file { '/tmp/school':
+        ensure  => 'file',
+        content => 'I love Puppet',
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0774',
+    }
 }
