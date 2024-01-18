ef0376df8f8b98798ef726f8985faba835a97126
ilias <iliaschokairi@gmail.com>
2024-01-18 08:25:41 +0100

    lets'go

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,6 @@
 # manifest that kills a process named killmenow
 
 exec { "killmenow":
-    command  => "pkill -f killmenow",
-    path     => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-    provider => "shell",
-    onlyif   => "pgrep -f killmenow",
-    return_code => [0, 1],
+  command   => "pkill  killmenow",
+  path      => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
 }
