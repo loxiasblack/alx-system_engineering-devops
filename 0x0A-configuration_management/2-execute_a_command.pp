326be01b9921a8331edac8a8277445c66665704b
ilias <iliaschokairi@gmail.com>
2024-01-18 07:09:23 +0100

    try

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
 exec { "killmenow":
-    command => "pkill -f killmenow || true",
-    path => '/bin:/usr/bin:sbin:/usr/sbin',
+    command => "pkill -f killmenow",
+    path => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
 }
