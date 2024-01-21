c86e5670c5356f816e47ec964a4ef633dfaaa8be
ilias <iliaschokairi@gmail.com>
2024-01-21 15:27:27 -0800

    creat a puppet that config ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#SSH configuration file
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+}
