7b404fc298d53961e23a514b69e6aa46d407fa9d
loxiasblack <iliaschokairi@gmail.com>
2024-05-07 16:07:47 +0100

    use a lot of except and try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,15 @@ import requests
 def number_of_subscribers(subreddit):
     """my function get"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    try:
+        response = requests.get(url, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0
+    except requests.RequestException as e:
         return 0
