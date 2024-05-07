9bf0fb3ac9d03b5f6f7dbb406c0de07da882462a
loxiasblack <iliaschokairi@gmail.com>
2024-05-07 15:54:09 +0100

    get the number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """"""
+    """my function get"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url)
 
