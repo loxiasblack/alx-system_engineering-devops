bf96b4e2ef73a16901edb25233e342a2500aee88
loxiasblack <iliaschokairi@gmail.com>
2024-05-07 16:03:00 +0100

    allow_redirect false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """my function get"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
